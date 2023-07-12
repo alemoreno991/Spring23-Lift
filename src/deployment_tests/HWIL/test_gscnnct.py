d0424fe984928ba767ffdabba126897b4775b6bc
PeteL <petelealiieejr4@utexas.edu>
2023-07-12 12:57:19 -0500

    started serial deployment tests

--- src/deployment_tests/HWIL/test_gscnnct.py
+++ src/deployment_tests/HWIL/test_gscnnct.py
@@ -14,9 +14,9 @@ def main():
         print('[ERROR]: CAPTURE OBJECT DID NOT OPEN ... EXITING')
         exit(0)
     else:
-        print('[DEBUG]: CAPTURE OBJECT OPENNED STARTING READ PROCESS')
+        print('[DEBUG]: CAPTURE OBJECT OPENNED. STARTING READ PROCESS')
 
-    for ii in range(0,3):
+    for ii in range(0,6):
         _,frame = cap_obj.read()
         cv.imwrite('./gscnnct_bin/test_im{}.png'.format(ii),frame)
         print('[DEBUG]: IMAGE {} SAVED TO "./gscnnct_bin/". WAITING 5 SECONDS'.format(ii))
@@ -27,7 +27,7 @@ def main():
 
 
 def rtrv_gs_cnct():
-    gspline = 'udpsrc port={} caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! nvv4l2decoder ! videoconvert ! appsink'.format(dplyC.RTRVL_PORT)
+    gspline = 'udpsrc port={} caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! nvv4l2decoder ! videoconvert ! appsink'.format(dplyC.NTWRK_RTRVL_PORT)
     gscap = cv.VideoCapture( gspline, cv.CAP_GSTREAMER )
     return gscap
 
--- src/deployment_tests/HWIL/test_pktrtrvl.py
+++ src/deployment_tests/HWIL/test_pktrtrvl.py
@@ -1,3 +1,55 @@
 import deploymentConst as dplyC
-import serial
+import serial.tools.list_ports
+import time
 
+def main():
+    ports = serial.tools.list_ports.comports()
+
+    portsList = []
+    print("[DEBUG]: DISPLAYING PORT OPTIONS")
+    for port in ports:
+        portsList.append(str(port))
+        print(str(port))
+
+    port_name = dplyC.SERIAL_PORT_NAME
+    if port_name is None:
+        port_select = input("\nSELECT PORT FROM LIST: COM")
+        for ii in range(0,len(portsList)):
+            if portsList[ii].startswith( "COM" + str(port_select) ):
+                port_name = "COM" + str(port_select)
+                break
+
+    if not (port_name is None):
+        print("[DEBUG]: SELECTED PORT IS {}".format(port_name))
+    else:
+        print("[ERROR]: SELECTED PORT WAS NOT FOUND ... EXITING")
+        exit(0)
+
+    # FORMAT SERIAL CONNECTION
+    srl_obj = serial.Serial()
+    srl_obj.port = port_name
+    srl_obj.baudrate = dplyC.SERIAL_BAUDRATE
+    srl_obj.bytesize = dplyC.SERIAL_BYTESIZE
+    # srl_obj.parity
+    # srl_obj.stopbits
+    srl_obj.timeout = dplyC.SERIAL_TIMEOUT
+
+    
+    srl_obj.open()
+    if srl_obj.is_open:
+        print("[DEBUG]: SERIAL CONNECTION OPENNED")
+    else:
+        print("[DEBUG]: SERIAL CONNECTION DID NOT OPEN .. EXITING")
+        exit(0)
+
+    for ii in range(0,6):
+        packet_t = srl_obj.readline()
+        # TODO: PARSE THE PACKET WRT INCOMING DATA
+        time.sleep(5)
+
+    srl_obj.close()
+    print("[RESULT]: TEST CONCLUDED ... EXITING")
+    
+
+if __name__ == '__main__':
+    main()
