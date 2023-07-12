550ecf2350f7bda49b7e2bcad03b5991dae51ffd
PeteL <petelealiieejr4@utexas.edu>
2023-07-12 18:25:41 -0500

    finished deployment tests

--- src/deployment_tests/HWIL/test_pktrtrvl.py
+++ src/deployment_tests/HWIL/test_pktrtrvl.py
@@ -1,6 +1,7 @@
 import deploymentConst as dplyC
 import serial.tools.list_ports
 import time
+import struct
 
 def main():
     ports = serial.tools.list_ports.comports()
@@ -29,22 +30,38 @@ def main():
     srl_obj = serial.Serial()
     srl_obj.port = port_name
     srl_obj.baudrate = dplyC.SERIAL_BAUDRATE
-    srl_obj.bytesize = dplyC.SERIAL_BYTESIZE
-    # srl_obj.parity
-    # srl_obj.stopbits
-    srl_obj.timeout = dplyC.SERIAL_TIMEOUT
-
     
     srl_obj.open()
     if srl_obj.is_open:
-        print("[DEBUG]: SERIAL CONNECTION OPENNED")
+        print("[DEBUG]: SERIAL CONNECTION OPENNED. SAMPLING IN 5 SECOND INTERVALS")
     else:
         print("[DEBUG]: SERIAL CONNECTION DID NOT OPEN .. EXITING")
         exit(0)
 
     for ii in range(0,6):
-        packet_t = srl_obj.readline()
-        # TODO: PARSE THE PACKET WRT INCOMING DATA
+        packet = srl_obj.readline( )
+        packet_b = bytes.fromhex( packet )
+        stamp_t, hash_t, phase_t, time_t, lon_t, lat_t, wgs_t, heading_t, vel_t, pitch_t, roll_t, yaw_t, p_t, q_t, r_t, xacc_t, yacc_t, zacc_t = struct.unpack( '>fIHfIIIfffffffffff', packet_b )
+        print("######################################################################")
+        print("[DEBUG]: TIMESTAMP - {}".format(stamp_t))
+        print("[DEBUG]: HASH - {}".format(hash_t))
+        print("[DEBUG]: PHASE - {}".format(phase_t))
+        print("[DEBUG]: TIME - {}".format(time_t))
+        print("[DEBUG]: LONGITUDE - {}".format(lon_t))
+        print("[DEBUG]: LATITUDE - {}".format(lat_t))
+        print("[DEBUG]: WGS - {}".format(wgs_t))
+        print("[DEBUG]: HEADING - {}".format(heading_t))
+        print("[DEBUG]: VELOCITY - {}".format(vel_t))
+        print("[DEBUG]: PITCH - {}".format(pitch_t))
+        print("[DEBUG]: ROLL - {}".format(roll_t))
+        print("[DEBUG]: YAW - {}".format(yaw_t))
+        print("[DEBUG]: P - {}".format(p_t))
+        print("[DEBUG]: Q - {}".format(q_t))
+        print("[DEBUG]: R - {}".format(r_t))
+        print("[DEBUG]: X ACCELERATION - {}".format(xacc_t))
+        print("[DEBUG]: Y ACCELERATION - {}".format(yacc_t))
+        print("[DEBUG]: Z ACCELERATION - {}".format(zacc_t))
+        print("######################################################################")
         time.sleep(5)
 
     srl_obj.close()
